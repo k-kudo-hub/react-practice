4ce70ac6a3a9d0b06b2ae9dc097555cfcf314fbf
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-30 20:23:32 +0900

    State management and definition of methods to change state

--- learn-react-todo/src/App.jsx
+++ learn-react-todo/src/App.jsx
@@ -3,6 +3,14 @@ import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
   // state管理する変数名と、stateを更新する関数名
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, toggleFaceShowFlag] = useState(true);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickToggleShowFlag = () => {
+    toggleFaceShowFlag(!faceShowFlag);
+  }
   return (
     <>
       <h1 style={{ color: 'red' }}>Learn React</h1>
@@ -14,6 +22,11 @@ const App = () => {
         color="pink"
         message="元気です"
       />
+      <button　onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickToggleShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>（＾∇＾）</p>}
     </>
   );
 }

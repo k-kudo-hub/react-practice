fd39007e7a0fc6746a53f0ad556be29ab0dbf75a
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-30 20:20:55 +0900

    Set rendering root component.

--- learn-react-todo/src/App.jsx
+++ learn-react-todo/src/App.jsx
@@ -1,1 +1,13 @@
+import React, { useState } from 'react';
+import ColorfulMessage from './components/ColorfulMessage';
 
+const App = () => {
+  // state管理する変数名と、stateを更新する関数名
+  return (
+    <>
+      <h1 style={{ color: 'red' }}>Learn React</h1>
+    </>
+  );
+}
+
+export default App;
